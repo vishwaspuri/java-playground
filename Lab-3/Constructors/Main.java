bd391edcc5c8ce42f680e6aee7d51fb318ca591d
Vishwas Puri <vishwaspuri09@gmail.com>
2021-06-14 17:49:17 +0530

    Lab-3

--- Lab-3/Constructors/Main.java
+++ Lab-3/Constructors/Main.java
@@ -8,7 +8,12 @@ public class Main{
         Box box2 = new Box();
         box.display();
         box2.display();
+        // After the next line both box2 and box will point to the same memory. 
         box2 = box;
         box2.display();
+        box2.length = 10; // This change gets transmitted. 
+        box.display();
+        box2.display();
+        
     }
 }
